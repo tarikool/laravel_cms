441fa02635ce07e756b4f2ca85b9a49b4134c663
Tarikool <ijad.tarikool@gmail.com>
2020-07-05 13:46:23 +0600

    Initial setup

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,39 @@
+@extends('auth.passwords.email')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h6>Section 1</h6>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h6>video1</h6>
+                    </div>
+
+                    <div class="col-md-6">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h6>video2</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <h6>video2</h6>
+                            </div>
+
+                            <div class="col-md-6">
+                                <h6>video2</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <h6>video2</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <h6>Section 1</h6>
+            </div>
+        </div>
+    </div>
+@endsection
